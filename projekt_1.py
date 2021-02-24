a09b02b81e7555aa4f0c1f0bd78b0f8d8303f523
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2021-02-24 15:14:55 +0100

    code complete

--- projekt_1.py
+++ projekt_1.py
@@ -12,26 +12,21 @@ np_csv = wig20_input.to_numpy()         #numpy conversion
 values = np_csv[:,4]                    #values to compute
 
 macd_val = []
-prices_df = pandas.DataFrame(values)
-day12 = prices_df.ewm(span=12).mean()
-day26 = prices_df.ewm(span=26).mean()
-for i in range(26,i_size,1):            #computing macd values from 26 to the end, because we need 27 values in case to compute ema26
-    ema_12 = day12.iloc[i,0]
-    ema_26 = day26.iloc[i,0]
+for i in range(0,i_size,1):            #computing macd values from 26 to the end, because we need 27 values in case to compute ema26
+    ema_12 = emaCompute(12,i,values)
+    ema_26 = emaCompute(26,i,values)
     macd_val.append(ema_12 - ema_26)
 
 signal_val = []                         #computing macd signal values from 9 to the end, because we need 19 values in case to compute ema9
-signal_df = pandas.DataFrame(macd_val)
-day9 = signal_df.ewm(span=9).mean()
-for i in range(9, len(macd_val),1):
-    ema_9 = day9.iloc[i,0]
+for i in range(0, len(macd_val),1):
+    ema_9 = emaCompute(9,i,macd_val)
     signal_val.append(ema_9)
 
 x_axis = np.arange(0,len(signal_val))
 x_limit = [len(signal_val)- 100, len(signal_val)-1]         #limitations to plots
 y_limit = [-200,200]
 
-plt.plot(x_axis, macd_val[9:],'b', label="MACD")            #plotting MACD and MACD-signal on each other
+plt.plot(x_axis, macd_val,'b', label="MACD")            #plotting MACD and MACD-signal on each other
 plt.plot(x_axis, signal_val,'r', label="MACD-signal")       #red - MACD-signal, blue - MACD
 plt.title("MACD, MACD-signal")
 plt.xlabel("Indeks próbki")
@@ -42,7 +37,7 @@ axes.set_xlim(x_limit)
 axes.set_ylim(y_limit)
 plt.show()
 
-plt.plot(x_axis, values[35:])                               #plotting values in case to check the usage of the MACD
+plt.plot(x_axis, values)                               #plotting values in case to check the usage of the MACD
 plt.title("Values")
 axes = plt.gca()
 axes.set_xlim(x_limit)
--- projekt_1_handlers.py
+++ projekt_1_handlers.py
@@ -1,10 +1,10 @@
-def emaCompute(N,startIndex, values):           #I assume that if input is invalid return value is min_int
-    if(startIndex - N < 0): return float('-inf')
-    alfa = 2/(N+1)                              #settign up constants and sums variables
+def emaCompute(N,startIndex, values):
+    alfa = 2.0/(N+1.0)                              #settign up constants and sums variables
     den = 0
     cntr = 0
     mul = 1 - alfa
     for i in range(0,N+1,1):                    #computing counter and denominator in one loop, i=0 i_max = N
+        if(startIndex-i < 0): continue
         den += pow(mul,i)                       #den += (1 - alfa)^i
         cntr += pow(mul,i)*values[startIndex-i] #cntr += (1 - alfa)^i * p{i}
     return (cntr/den)
