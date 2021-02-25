6cc67ddb96c75d98f5dbb87000d3f1f6c379568e
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2021-02-25 11:07:38 +0100

    full project with visualization and alogrithm

--- projekt_1.py
+++ projekt_1.py
@@ -1,6 +1,6 @@
 #%matplotlib inline                      #handling jupyter error
 import pandas
-from projekt_1_handlers import emaCompute
+from projekt_1_handlers import assetAlg, computeAsset, emaCompute, menageStock, smmCompute, wprCompute
 import matplotlib.pyplot as plt
 import numpy as np
 wig20_input = pandas.read_csv('wig20_d.csv')            #Data, Otwarcie, Najnizszy, Najwyzszy,  Zamkniecie, Wolumen
@@ -10,35 +10,48 @@ i_size = wig20_input.size // c_size     #input size(rows)
 
 np_csv = wig20_input.to_numpy()         #numpy conversion
 values = np_csv[:,4]                    #values to compute
-
 macd_val = []
+signal_val = []
+lowest = np_csv[:,3]
+highest = np_csv[:,2]
+
+def showPlot():
+    plt.plot(x_axis, macd_val,'b', label="MACD")            #plotting MACD and MACD-signal on each other
+    plt.plot(x_axis, signal_val,'r', label="MACD-signal")       #red - MACD-signal, blue - MACD
+    plt.title("MACD, MACD-signal")
+    plt.xlabel("Indeks próbki")
+    plt.ylabel("Wskaźnik")
+    plt.legend(loc="upper left")
+    axes = plt.gca()
+    axes.set_xlim(x_limit)
+    axes.set_ylim(y_limit)
+    plt.show()
+
+    plt.plot(x_axis, values)                               #plotting values in case to check the usage of the MACD
+    plt.title("Values")
+    axes = plt.gca()
+    axes.set_xlim(x_limit)
+    plt.show()
+
 for i in range(0,i_size,1):            #computing macd values from 26 to the end, because we need 27 values in case to compute ema26
     ema_12 = emaCompute(12,i,values)
     ema_26 = emaCompute(26,i,values)
     macd_val.append(ema_12 - ema_26)
-
-signal_val = []                         #computing macd signal values from 9 to the end, because we need 19 values in case to compute ema9
+                                        #computing macd signal values from 9 to the end, because we need 19 values in case to compute ema9
 for i in range(0, len(macd_val),1):
     ema_9 = emaCompute(9,i,macd_val)
     signal_val.append(ema_9)
 
+
 x_axis = np.arange(0,len(signal_val))
 x_limit = [len(signal_val)- 100, len(signal_val)-1]         #limitations to plots
 y_limit = [-200,200]
+                                                        
+#showPlot()                                              #wrapped plotShowing into a function, normally I would consider making a function with parameters, however I only need them
+                                                        #for simple visualization, therefore I don't want to focus on this too much
 
-plt.plot(x_axis, macd_val,'b', label="MACD")            #plotting MACD and MACD-signal on each other
-plt.plot(x_axis, signal_val,'r', label="MACD-signal")       #red - MACD-signal, blue - MACD
-plt.title("MACD, MACD-signal")
-plt.xlabel("Indeks próbki")
-plt.ylabel("Wskaźnik")
-plt.legend(loc="upper left")
-axes = plt.gca()
-axes.set_xlim(x_limit)
-axes.set_ylim(y_limit)
-plt.show()
+computeAsset(macd_val,signal_val,values)                #wrapped asset computing into a function with 3 parameters which are computed values for macd,signal and closing values
+                                                        #this function is computing asset using only MACD, more in projekt_1_handlers.py file
 
-plt.plot(x_axis, values)                               #plotting values in case to check the usage of the MACD
-plt.title("Values")
-axes = plt.gca()
-axes.set_xlim(x_limit)
-plt.show()
+assetAlg(values,highest,lowest,macd_val)                #wrapped my asset computing algorithm into function using 4 parameters, closing values, highest values, lowest values per day
+                                                        #and computed MACD, more about this in projekt_1_handlers.py
--- projekt_1_handlers.py
+++ projekt_1_handlers.py
@@ -1,10 +1,114 @@
+import pandas
 def emaCompute(N,startIndex, values):
-    alfa = 2.0/(N+1.0)                              #settign up constants and sums variables
+    alfa = 2.0/(N+1.0)                              #setting up constants and sums variables
     den = 0
     cntr = 0
     mul = 1 - alfa
     for i in range(0,N+1,1):                    #computing counter and denominator in one loop, i=0 i_max = N
-        if(startIndex-i < 0): continue
+        if(startIndex-i < 0): break
         den += pow(mul,i)                       #den += (1 - alfa)^i
         cntr += pow(mul,i)*values[startIndex-i] #cntr += (1 - alfa)^i * p{i}
     return (cntr/den)
+
+def menageStock(signal):                        #function to determine where the macd and signal lines cut
+    Buy = []
+    Sell = []
+    flag = -1
+
+    for i in range(0,len(signal)):
+        if signal['MACD'][i] > signal['Signal Line'][i] and flag != 1:
+            Buy.append(i+35)
+            flag = 1
+        elif signal['MACD'][i] < signal['Signal Line'][i] and flag == 1:
+            Sell.append(i+35)
+            flag = 0
+    return (Buy, Sell)                          #returns lists of values indexes where the lines signs to buy/sell
+
+def wprCompute(N,startIndex,values, highest, lowest):            #function to compute wpr value for N period
+    hi = float('-inf')
+    close = values[startIndex]
+    low = float('inf')
+    for i in range(0,N+1,1):
+        new_h = highest[startIndex-i]
+        new_l = lowest[startIndex-i]
+        if(new_h > hi): hi = new_h
+        if(new_l < low): low = new_l
+    return ((hi - close)/(hi - low) * -100)
+
+def smmCompute(N,startIndex,values,smmSum):     #function to compute smm value for N period
+    if(len(smmSum) == 0): return values[startIndex]
+    sum = values[startIndex]
+    sum_b = smmSum[startIndex-1]
+    for i in range(0,N,1):
+        if(startIndex-i < 0):break
+        sum += values[startIndex-i]
+    return ((sum - sum_b) / N)
+
+def computeAsset(macd_val, signal_val,values):
+    df = pandas.DataFrame()                                 #simple algorithm to compute gains for earlier computed MACD values
+    df['MACD'] = macd_val[35:]
+    df['Signal Line'] = signal_val[35:]
+    df['Close Value'] = values[35:]
+    Buy, Sell = menageStock(df)
+
+    Times = (Buy + Sell)
+    Times.sort()
+
+    asset_MACD = 1000
+    cur_value = 0
+    max_value = 0
+    i = 0
+
+    print('')
+    print("Algorytm bazujący na przecięciach linii MACD oraz MACD-signal.")
+    print("Kapitał początkowy: ", asset_MACD)
+
+    for x in range(0,len(Times)):
+        if(asset_MACD):
+            cur_value = asset_MACD / values[Times[x]]       #stock for 1000j
+            asset_MACD = 0
+        elif(cur_value):
+            asset_MACD = cur_value * values[Times[x]]       #j we can cash out
+            if(asset_MACD > max_value): max_value = asset_MACD; i = Times[x]
+            cur_value = 0
+            #print(asset_MACD)
+        if(x == len(Times)-2 and asset_MACD): break
+
+    print("Kapitał: ", asset_MACD)
+    print("Maxymalny kapitał kiedykolwiek: ", max_value, " w dniu próbki nr.", i)
+    print('')
+
+def assetAlg(values, highest, lowest, macd_val):
+    #algorithm for auto buy/sell mechanism on stock
+    #using WPR, MACD, SMM to compute through each day and decide wheter buy or sell
+    wpr_val = []
+    smma_val = []
+    for i in range(0,len(values),1):
+        wpr_val.append(wprCompute(14,i,values,highest,lowest))
+        smma_val.append(smmCompute(50,i,values,smma_val))
+
+    asset = 1000
+    cur_value = 0
+    last_value = asset
+    max_value = 0
+    i = 0
+    print('')
+    print("Algorytm bazujący na MACD jako wskaźniku trendu oraz pomocniczo WPR oraz SMMA, jako wskaźniki zawyżonych i zaniżonych wartości.")
+    print("Kapitał początkowy: ", asset)
+
+    for x in range(35,len(values),1):
+        if(asset):                  #if we can invest
+            if(values[x] > smma_val[x] and macd_val[x] < 0 and wpr_val[x-1] < -80 and wpr_val[x] > -80):
+                cur_value = asset / values[x]
+                asset = 0
+        else:                       #if we need to cash out
+            if(values[x] < smma_val[x] and macd_val[x] > 0 and wpr_val[x-1] > -20 and wpr_val[x] < -20):
+                asset = cur_value * values[x]
+                cur_value = 0
+                last_value = asset
+                if(max_value < asset): max_value = asset; i = x
+    if(asset == 0):
+        asset = last_value
+    print("Kapitał: ", asset)
+    print("Maxymalny kapitał kiedykolwiek: ", max_value, " w dniu próbki nr.", i)
+    print('')
